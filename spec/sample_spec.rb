307218e873a796d40c4c08252b08b2ea870a7b3e
Katie Wright <katie.wrght@gmail.com>
2024-01-29 12:17:10 +1300

    add flakey tests

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe "A sample test" do
   it "fails" do
     expect(42).to eql(13)
   end
+  it "might flake" do
+    expect(rand(0...2)).to eq 1
+  end
+  it "might also flake" do
+    expect(rand(0...2)).to eq 1
+  end
 end
