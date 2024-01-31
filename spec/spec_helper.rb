a7bef81bfe20b92c193c7ff3f8c7923390ffbd42
Katie Wright <katie.wrght@gmail.com>
2024-02-01 09:56:32 +1300

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,10 @@
  require "buildkite/test_collector"
  # Localhost
-# Buildkite::TestCollector.configure(
-#   hook: :rspec,
-#   token: "yDE9T1nBjNDSxaMjbn3sPcbS",
-#   url: "http://analytics-api.buildkite.localhost/v1/uploads"
-# )
-# RSpec.configure do |config|
-#   config.add_formatter('RspecJunitFormatter', "artifacts/rspec-#{ENV['BUILDKITE_JOB_ID']}.xml")
-# end
+Buildkite::TestCollector.configure(
+  hook: :rspec,
+  token: "yDE9T1nBjNDSxaMjbn3sPcbS",
+  url: "http://analytics-api.buildkite.localhost/v1/uploads"
+)
+
 # Production
-Buildkite::TestCollector.configure(hook: :rspec)
+# Buildkite::TestCollector.configure(hook: :rspec)
