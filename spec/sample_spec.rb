355a46d09531dd699c6507500361aa73631d675f
Katie Wright <katie.wrght@gmail.com>
2024-02-28 11:19:09 +1300

    add test with 1% chance of flaking

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -13,12 +13,19 @@ RSpec.describe "A sample test" do
   it "I have a 20% chance of failing" do
     expect(rand(0...25)).to be <=20
   end
+
   it "I have a 10% chance of failing" do
     expect(rand(0...50)).to be <=45
   end
+
+  it "I have a 1% chance of failing" do
+    expect(rand(0...99)).to be <=99
+  end
+
   it "I have a 66% chance of failing" do
     expect(rand(0...3)).to eq 1
   end
+
   it "I have a 50% chance of failing" do
     expect(rand(0...2)).to eq 1
   end
