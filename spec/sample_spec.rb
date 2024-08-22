e7466f11453ba34f4cb5660e6e4730982e5eb1c3
Katie Wright <katie.wrght@gmail.com>
2024-08-22 12:47:59 +1200

    remove tests

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -9,19 +9,19 @@ RSpec.describe "A sample test" do
     expect(rand(0...4)).to be < 3
   end
 
-  it "I have a 10% chance of failing" do
+  xit "I have a 10% chance of failing" do
     expect(rand(0...10)).to be < 9
   end
 
-  it "I have a 1% chance of failing" do
+  xit "I have a 1% chance of failing" do
     expect(rand(0...100)).to be < 99
   end
 
-  it "I have a 33% chance of failing" do
+  xit "I have a 33% chance of failing" do
     expect(rand(0...3)).to eq 1
   end
 
-  it "I have a 50% chance of failing" do
+  xit "I have a 50% chance of failing" do
     expect(rand(0...2)).to eq 1
   end
 end
