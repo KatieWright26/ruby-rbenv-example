75ed53bc6d4bcc7442e1bdf8d71a4d7b9d940106
Katie Wright <katie.wrght@gmail.com>
2024-09-09 11:47:46 +1200

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
  # Localhost
 Buildkite::TestCollector.configure(
   hook: :rspec,
-  token: "DAw8yiRatv23ajs8fGtekraX",
+  token: "fwxr2GKy4y21FPiSKXeYkihm",
   url: "http://analytics-api.buildkite.localhost/v1/uploads"
 )
 
